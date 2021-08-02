3079fd7ffae31ac5e347ffff12b077ea14a99280
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-08-02 09:26:25 -0400

    Initial setup of cookies and user sign on with tracking of the session

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
+var User = require('../models/user');
 var router = express.Router();
+
+router.use.json();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/signup', function(req,res,next) {
+  User.findOne({username: req.body.username})
+  .then((user) => {
+    if (user != null) {
+      var err = new Error('User ' + req.body.username + ' already exists!');
+      err.status = 403;
+      next(err); 
+    }
+    else {
+      return User.create({
+        username: req.body.username,
+        password: req.body.password
+      });
+    }
+  })
+  .then((user) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({status: 'Registration Successful', user: user});
+  }, (err) => next(err))
+  .catch((err) => next(err));
 });
 
 module.exports = router;

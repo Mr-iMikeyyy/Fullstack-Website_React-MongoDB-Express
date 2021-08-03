d1021a1eba986e508aad9ceea6243b2c0b03f691
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-08-03 13:56:42 -0400

    Users can change/delete comments, admin verification

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authenticate = require('../authenticate');
 
-const Dishes = require('../models/dishes')
+const Dishes = require('../models/dishes');
+const user = require('../models/user');
 
 const dishRouter = express.Router();
 
@@ -19,7 +20,7 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err))
 })
-.post(authenticate.verifyUser, (req,res,next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
     Dishes.create(req.body)
     .then((dish) => {
         console.log('Dish Created: ', dish);
@@ -29,11 +30,11 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put(authenticate.verifyUser, (req,res,next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes');
 })
-.delete(authenticate.verifyUser, (req,res,next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
     Dishes.remove({})
     .then((resp) => {
         res.statusCode = 200;
@@ -54,11 +55,11 @@ dishRouter.route('/:dishId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post(authenticate.verifyUser, (req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   res.statusCode = 403;
   res.end('POST operation not supported on /dishes/'+ req.params.dishId);
 })
-.put(authenticate.verifyUser, (req, res, next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.findByIdAndUpdate(req.params.dishId, {
         $set: req.body
     }, { new: true})
@@ -69,7 +70,7 @@ dishRouter.route('/:dishId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.findByIdRemove(req.params.dishId)
     .then((dish) => {
         res.statusCode = 200;
@@ -125,7 +126,7 @@ dishRouter.route('/:dishid/comments')
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes/' + req.params.dishId + '/comments');
 })
-.delete(authenticate.verifyUser, (req,res,next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
     Dishes.findById(req.params.dishid)
     .then((dish) => {
         if(dish != null) {
@@ -176,7 +177,18 @@ dishRouter.route('/:dishId/comments/:commentId')
   res.end('POST operation not supported on /dishes/'+ req.params.dishId + '/comments/' + req.params.commentId);
 })
 .put(authenticate.verifyUser, (req, res, next) => {
+
     Dishes.findById(req.params.dishId)
+    .then((dish) => {
+        if (req.user.id.equals(dish.comments.id(req.params.commentId).author._id)) {
+            next();
+        }
+        else {
+            err = new Error('You are not the author of this comment, cannot delete');
+            err.status = 403;
+            return next(err);
+        }
+    })
     .then((dish) => {
         if(dish != null && dish.comments.id(req.params.commentId) != null) {
             if (req.body.rating) {
@@ -210,6 +222,16 @@ dishRouter.route('/:dishId/comments/:commentId')
 })
 .delete(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishid)
+    .then((dish) => {
+        if (req.user.id.equals(dish.comments.id(req.params.commentId).author._id)) {
+            next();
+        }
+        else {
+            err = new Error('You are not the author of this comment, cannot delete');
+            err.status = 403;
+            return next(err);
+        }
+    })
     .then((dish) => {
         if(dish != null && dish.comments.id(req.params.commentId) != null) {
             dish.comments.id(req.params.commentId).remove();
